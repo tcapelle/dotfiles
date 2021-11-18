d5f677d7d243863ae1f33b96ae60e901c85f5721
Thomas Capelle <thomas.capelle@steady-sun.com>
2021-11-18 20:45:11 +0100

    fix colors in ls

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -32,6 +32,6 @@ fi
 
 # dircolors
 
-if [[ "$(tput colors)" == "256" ]]; then
-    eval $(dircolors =(cat ~/.shell/plugins/dircolors-solarized/dircolors.256dark ~/.shell/dircolors.extra))
-fi
+#if [[ "$(tput colors)" == "256" ]]; then
+#    eval $(dircolors =(cat ~/.shell/plugins/dircolors-solarized/dircolors.256dark ~/.shell/dircolors.extra))
+#fi
