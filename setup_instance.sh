1571d223a7de5e244c8446ac8c8c324741b8e6a7
Thomas Capelle <thomas.capelle@steady-sun.com>
2023-10-30 12:42:42 +0000

    dotfiles optional

---
+++ setup_instance.sh
@@ -0,0 +1,32 @@
+## Remove dot files
+rm -rf ~/.jupyter
+rm ~/.zshrc
+rm ~/.bashrc
+
+## Install ubuntu packages
+sudo apt-get update -y
+sudo apt install wget vim tmux zsh -y
+
+## Install dotfiles
+echo "Do you want to install Cape's dotfiles? (yes/no)"
+read install_dotfiles
+if [ "$install_dotfiles" = "yes" ]; then
+    sh install
+fi
+
+## Download Miniforge
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+
+## Install Anaconda
+echo "Installing Anaconda"
+./Miniforge3-Linux-x86_64.sh -b
+chmod +x Miniforge3-Linux-x86_64.sh
+source ~/.bashrc
+conda init zsh
+conda init bash
+
+# Create PyTorch env
+echo "Creating a PyTorch Env"
+conda create -n pt python=3.10
+conda activate pt
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 jupyterlab -c pytorch -y
