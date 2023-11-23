078d9e61656e4676defaee224abcb46368b8c4e5
Thomas Capelle <tcapelle@pm.me>
2023-11-23 18:58:09 +0100

    Add SSH read and isolate flash_attn install

--- setup_instance.sh
+++ setup_instance.sh
@@ -8,6 +8,19 @@ rm ~/.bashrc
 sudo apt-get update -y
 sudo apt install wget vim tmux zsh -y
 
+## Paste SSH Key
+echo "Do you want to paste an SSH key?"
+read ssh_key
+
+if [[ -n "$ssh_key" ]]; then
+    echo "$ssh_key" > ~/.ssh/id_cape
+    chmod 600 ~/.ssh/id_cape
+    echo "SSH key has been stored in ~/.ssh/id_cape"
+else
+    echo "No SSH key was provided."
+fi
+
+
 ## Install dotfiles
 echo "Do you want to install Cape's dotfiles? (yes/no)"
 read install_dotfiles
@@ -33,5 +46,13 @@ echo "Creating a PyTorch Env"
 conda create -n pt "python<3.11"
 conda activate pt
 python -m pip install torch torchvision packaging
-python -m pip install transformers datasets peft bitsandbytes jupyterlab wandb flash_attn
+python -m pip install transformers datasets peft bitsandbytes jupyterlab wandb ninja
+
+# Install Flash Attention from tri-dao
+echo "Do you want to install flash_attn? (yes/no)"
+read install_flash
+if [ "$install_flash" = "yes" ]; then
+    echo "This may take 3~5 minutes"
+    python -m pip install flash-attn --no-cache --no-build-isolation
+fi
 python -c "import torch; print(torch.cuda.is_available())"
